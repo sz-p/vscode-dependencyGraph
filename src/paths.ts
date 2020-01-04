5ddf102d796cf541943315c9c834a11b77e4dc84
sz-p <sz_p@outlook.com>
2020-01-04 15:53:21 +0800

    [feat]: add explorer view

---
+++ src/paths.ts
@@ -0,0 +1,8 @@
+import * as path from 'path';
+const framegraphSVG = path.resolve(__dirname, '../resources/framegraph.svg');
+const framegraphPNG = path.resolve(__dirname, '../resources/framegraph.png');
+
+export {
+  framegraphSVG,
+  framegraphPNG
+};
