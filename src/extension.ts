4bcb98e09003f65da20eeee1c3b4736f1109a44a
sz-p <sz_p@outlook.com>
2020-08-05 00:00:00 +0800

    [feat]: get dependencyTree

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,14 @@
 import * as vscode from 'vscode';
 import * as paths from './paths';
 import { helloWorld } from './helloworld';
+import { readfile } from './fileLoader/loadFiles';
 import { createView } from './openWebView';
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('init-extension');
 	createView();
 	context.subscriptions.push(helloWorld);
+	context.subscriptions.push(readfile);
 }
 
 // this method is called when your extension is deactivated
