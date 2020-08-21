6ba9364b6d4d62ecc49eac8a7cb13382cea0f716
sz-p <sz_p@outlook.com>
2020-08-21 15:08:34 +0800

    feat: treeData tree

--- src/webView/src/components/treeView/renderTree.js
+++ src/webView/src/components/treeView/renderTree.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
-import { zoom, getTreeData, getDOMRect, getPath } from './treeMethods';
+
+import { getDOMRect, renderTreeView, zoom, getTreeData } from './treeMethods';
 const CIRCLE_R = 8;
 const PADDING = {
 	LEFT: 100,
@@ -8,43 +9,14 @@ const PADDING = {
 	TOP: 20
 };
 const NODE_TEXT_OFFSET_X = 13;
+const DURATION_TIME = 750;
+export const renderTree = function(dom, data) {
+	const { width, height } = getDOMRect(dom);
 
-function renderTreeView(dom, width, height, treeData) {
 	const svgBox = d3.select(dom).append('svg').attr('width', width).attr('height', height);
 	const svg = svgBox.append('g').attr('transform', 'translate(' + PADDING.LEFT + ',' + PADDING.TOP + ')');
 
 	svgBox.call(zoom(svg));
 
-	const links = svg
-		.selectAll('.link')
-		.data(treeData.descendants().slice(1))
-		.enter()
-		.append('path')
-		.attr('class', 'link')
-		.attr('d', (d) => getPath(d));
-
-	const nodes = svg
-		.selectAll('.node')
-		.data(treeData.descendants())
-		.enter()
-		.append('g')
-		.attr('class', 'node')
-		.attr('transform', (d) => 'translate(' + d.y + ',' + d.x + ')');
-
-	nodes.append('circle').attr('r', CIRCLE_R);
-
-	nodes
-		.append('text')
-		.style('text-anchor', (d) => {
-			return d.children ? 'end' : 'start';
-		})
-		.attr('x', (d) => {
-			return d.children ? -NODE_TEXT_OFFSET_X : NODE_TEXT_OFFSET_X;
-		})
-		.text((d) => d.data.name);
-}
-export const renderTree = function(dom, data) {
-	const { width, height } = getDOMRect(dom);
-	const treeData = getTreeData(data, width, height, PADDING);
-	renderTreeView(dom, width, height, treeData);
+	renderTreeView(svg, data, width, height, { CIRCLE_R, PADDING, NODE_TEXT_OFFSET_X, DURATION_TIME });
 };
--- src/webView/src/components/treeView/treeMethods.js
+++ src/webView/src/components/treeView/treeMethods.js
@@ -1,22 +1,6 @@
 import * as d3 from 'd3';
 
-export const zoom = function(svg) {
-	const zoomed = function() {
-		const transform = d3.event.transform;
-		svg.attr('transform', transform);
-	};
-	return d3.zoom().on('zoom', zoomed);
-};
-export const getTreeData = function(data, width, height, PADDING) {
-	const treeData = d3.tree().size([ height - PADDING.LEFT - PADDING.RIGHT, width - PADDING.BOTTOM - PADDING.TOP ])(
-		d3.hierarchy(data)
-	);
-	return treeData;
-};
-export const getDOMRect = function(dom) {
-	return dom.getClientRects()[0];
-};
-export const getPath = function(d) {
+const getPath = function(d) {
 	return (
 		'M' +
 		d.y +
@@ -35,4 +19,86 @@ export const getPath = function(d) {
 		',' +
 		d.parent.x
 	);
+};
+
+const clickNode = function(renderTreeView, svg, data, width, height, options) {
+	return function(d) {
+		if (d.data.children) {
+			d.data._children = d.data.children;
+			d.data.children = null;
+		} else {
+			d.data.children = d.data._children;
+			d.data._children = null;
+		}
+		renderTreeView(svg, data, width, height, options);
+	};
+};
+
+export const zoom = function(svg) {
+	const zoomed = function() {
+		const transform = d3.event.transform;
+		svg.attr('transform', transform);
+	};
+	return d3.zoom().on('zoom', zoomed);
+};
+
+export const getTreeData = function(data, width, height, PADDING) {
+	const treeData = d3.tree().size([ height - PADDING.LEFT - PADDING.RIGHT, width - PADDING.BOTTOM - PADDING.TOP ])(
+		d3.hierarchy(data)
+	);
+	return treeData;
+};
+export const getDOMRect = function(dom) {
+	return dom.getClientRects()[0];
+};
+
+const fixDepth = function(nodes) {
+	nodes.eachBefore((d) => {
+		d.y = d.depth * 180;
+	});
+};
+const stashOldPosition = function(nodes) {
+	nodes.eachBefore((d) => {
+		d.x0 = d.x;
+		d.y0 = d.y;
+	});
+};
+export const renderTreeView = function(svg, data, width, height, options) {
+	const { NODE_TEXT_OFFSET_X, CIRCLE_R, PADDING, DURATION_TIME } = options;
+	const treeData = getTreeData(data, width, height, PADDING);
+  fixDepth(treeData)
+	const linksData = svg.selectAll('.link').data(treeData.descendants().slice(1), (d) => {
+		d.data.name;
+	});
+
+	linksData.exit().remove();
+
+	const linksDom = linksData
+		.enter()
+		.append('path')
+		.attr('class', 'link')
+		.transition()
+		.duration(DURATION_TIME)
+		.attr('d', (d) => getPath(d));
+
+	const nodesData = svg.selectAll('.node').data(treeData.descendants(), (d) => {
+		d.data.name;
+	});
+	nodesData.exit().remove();
+
+	const nodesDom = nodesData.enter().append('g').attr('class', 'node');
+	nodesDom.transition().duration(DURATION_TIME).attr('transform', (d) => 'translate(' + d.y + ',' + d.x + ')');
+	nodesDom
+		.append('circle')
+		.attr('r', CIRCLE_R)
+		.on('click', clickNode(renderTreeView, svg, data, width, height, options))
+		.style('cursor', (d) => (d.children ? 'pointer' : 'auto'));
+
+	nodesDom
+		.append('text')
+		.style('text-anchor', (d) => {
+			return d.children ? 'end' : 'start';
+		})
+		.attr('x', (d) => (d.children ? -NODE_TEXT_OFFSET_X : NODE_TEXT_OFFSET_X))
+		.text((d) => d.data.name);
 };
