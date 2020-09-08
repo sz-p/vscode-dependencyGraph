c3eb1b7c4ef34cb2b28e54b2afc77f51247bb4ad
sz-p <sz_p@outlook.com>
2020-09-08 11:10:52 +0800

    [feat]: throttle focus on node

--- src/webView/src/components/treeView/treeView.jsx
+++ src/webView/src/components/treeView/treeView.jsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import { useRef, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { D3Tree } from './renderTree';
+import { throttle } from '../../utils/utils';
 import "./treeView.css"
 
 const tree = new D3Tree();
+const focusOnNode = throttle(tree.focusOnNode.bind(tree), tree.DURATION_TIME);
 
 const treeView = function (props) {
   const { dependencyTreeData, focusOn, assetsBaseURL } = props;
   const chartArea = useRef();
   useEffect(() => { if (dependencyTreeData && assetsBaseURL) { tree.init(chartArea.current, dependencyTreeData, assetsBaseURL); tree.update() } }, [dependencyTreeData, assetsBaseURL])
-  useEffect(() => { if (focusOn) tree.focusOnNode(focusOn.fileData) }, [focusOn])
+  useEffect(() => { if (focusOn) focusOnNode(focusOn.fileData) }, [focusOn])
   return (<div className="treeView" ref={chartArea}></div>)
 }
 const mapStateToProps = (state) => {
