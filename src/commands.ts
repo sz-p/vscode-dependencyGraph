ecddd9cd8d37fda2654e2e53f57d0ccebef46a75
sz-p <sz_p@outlook.com>
2020-09-11 00:00:00 +0800

    refactor: commands

--- src/commands.ts
+++ src/commands.ts
@@ -1,50 +1,37 @@
 import * as vscode from 'vscode';
-import { getDependencyTreeData } from './data-dependencyTree/data-dependencyTree';
-import { DependenciesTreeProvider } from './view-dependencyTree/DependenciesTreeProvider';
+import { getDependencyTreeData, statusCallBack } from './data-dependencyTree/data-dependencyTree';
 import { createView } from './web-dependencyTree/openWebView';
-import { postMessage } from './utils/postMessageToWebView';
-import { MESSAGE_FOCUS_ON_NODE } from './utils/messagesKeys';
+import { postMessageCatchError } from './utils/message/postMessageToWebView';
+import { MESSAGE_FOCUS_ON_NODE } from './utils/message/messagesKeys';
 import { reOpenWebView } from './web-dependencyTree/reOpenWebView';
+import { renderTreeView } from './view-dependencyTree/renderTreeView';
+import { onError } from './utils/error/onError';
+import { NO_DEPENDENCY_TREE_DATA } from './utils/error/errorKey';
 let message = 0;
-
-export let dependencyTreeData = getDependencyTreeData();
-
-export const command_readFile = vscode.commands.registerCommand('framegraph.readfile', () => {
-	if (!dependencyTreeData) {
-		vscode.window.showInformationMessage('No dependency');
-		return false;
-	}
-	vscode.window.registerTreeDataProvider(
-		'framegraphExplorer-DependencyTree',
-		new DependenciesTreeProvider(dependencyTreeData)
-	);
-});
 
 export const command_openView = vscode.commands.registerCommand('framegraph.openView', () => {
 	createView();
 });
 
-export const command_postMessage = vscode.commands.registerCommand('framegraph.postMessage', () => {
-	postMessage({ key: 'postMessageTest', value: message++ });
-});
+// export const command_postMessage = vscode.commands.registerCommand('framegraph.postMessage', () => {
+// 	postMessageCatchError({ key: 'postMessageTest', value: message++ });
+// });
 
 export const command_focusOnNode = vscode.commands.registerCommand('framegraph.focusOnNode', (fileName, fileData) => {
-	postMessage({ key: MESSAGE_FOCUS_ON_NODE, value: { fileName, fileData } });
+	postMessageCatchError({ key: MESSAGE_FOCUS_ON_NODE, value: { fileName, fileData } });
 });
 export const command_reOpenView = vscode.commands.registerCommand('framegraph.reOpenView', (fileName, fileData) => {
-	if (dependencyTreeData) {
-		reOpenWebView(dependencyTreeData);
+	if (global.dependencyTreeData) {
+		reOpenWebView(global.dependencyTreeData);
 	} else {
-		console.error('no dependencyTreeData');
+		onError(NO_DEPENDENCY_TREE_DATA);
 	}
 });
 export const command_refreshFile = vscode.commands.registerCommand('framegraph.refreshFile', (fileName, fileData) => {
-	dependencyTreeData = getDependencyTreeData();
-	if (dependencyTreeData) {
-		vscode.window.registerTreeDataProvider(
-			'framegraphExplorer-DependencyTree',
-			new DependenciesTreeProvider(dependencyTreeData)
-		);
+	// no catch error may be webview is closed
+	global.dependencyTreeData = getDependencyTreeData(statusCallBack);
+	if (global.dependencyTreeData) {
+		renderTreeView(global.dependencyTreeData);
 	}
 });
-export const allCommands = [ command_readFile, command_openView, command_postMessage, command_reOpenView ];
+export const allCommands = [ command_openView, command_reOpenView ];
