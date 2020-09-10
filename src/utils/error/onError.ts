6f5602b860a765844b49a9ef98541ffbe988b9ee
sz-p <sz_p@outlook.com>
2020-09-11 00:00:00 +0800

    feat: on error get message

--- src/utils/error/onError.ts
+++ src/utils/error/onError.ts
@@ -1,7 +1,15 @@
 import { ErrorKey } from './errorKey';
 import { showMessage } from '../showMessage';
-export const onError = function(errorKey: ErrorKey, message: string, callback?: Function, callbackArg?: any) {
-	const messageText = `${errorKey}: ${message}`;
+import { getText } from '../../i18n/i18n';
+
+const getErrorMessage = function(errorKey: ErrorKey) {
+	return getText(errorKey as string);
+};
+export const onError = function(errorKey: ErrorKey, message?: string, callback?: Function, callbackArg?: any) {
+	let messageText = `${errorKey}: ${getErrorMessage(errorKey)}`;
+	if (message) {
+		messageText = messageText + `<br/>` + message;
+	}
 	showMessage(messageText);
 	if (callback) {
 		callback(callbackArg);
