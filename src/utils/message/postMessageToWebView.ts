e2ca108066576fff18597503bd4cccce888f718c
sz-p <sz_p@outlook.com>
2020-09-11 11:27:36 +0800

    feat: make post message  catch error

--- src/utils/message/postMessageToWebView.ts
+++ src/utils/message/postMessageToWebView.ts
@@ -1,6 +1,16 @@
+import { NO_WEBVIEW_PANEL } from '../error/errorKey';
+import { onError } from '../error/onError';
+
 import { MsgToWebView } from './message';
 export const postMessage = function(msg: MsgToWebView) {
 	if (global.webViewPanel) {
 		global.webViewPanel.webview.postMessage(msg);
+	}
+};
+export const postMessageCatchError = function(msg: MsgToWebView) {
+	if (global.webViewPanel) {
+		global.webViewPanel.webview.postMessage(msg);
+	} else {
+		onError(NO_WEBVIEW_PANEL);
 	}
 };
