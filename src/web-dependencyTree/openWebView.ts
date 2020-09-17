6a8022ad4417d3b2b504015c2bb384d2d2133a93
sz-p <sz_p@outlook.com>
2020-09-18 00:00:00 +0800

    feat: click open web view aways open webview

--- src/web-dependencyTree/openWebView.ts
+++ src/web-dependencyTree/openWebView.ts
@@ -16,9 +16,11 @@ import {
 } from '../utils/message/messagesKeys';
 import { DependencyTreeData } from '../data-dependencyTree/dependencyTreeData';
 import { createWebviewPanel } from '../initExtension';
-import { getCurrentFolderPath } from '../utils//utils';
-import { onError } from '../utils/error/onError';
-import { NO_FOLDER } from '../utils/error/errorKey';
+import { getCurrentFolderPath } from '../data-dependencyTree/dependencyTreeMethods';
+import { getEntryFilePath } from '../utils/config';
+
+import { statusMsgGetFolderPath, statusMsgGetEntryFile } from '../utils/message/messages';
+
 /**
  * 从某个HTML文件读取能被Webview加载的HTML内容
  * @param {*} templatePath 相对于插件根目录的html文件绝对路径
@@ -46,7 +48,7 @@ export const createView = function(): void {
 	}
 };
 
-export const reOpenWebView = function(dependencyTreeData: DependencyTreeData) {
+export const reOpenWebView = function(dependencyTreeData: DependencyTreeData | undefined) {
 	const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
 	if (global.webViewPanel) {
 		// 如果我们已经有了一个面板，那就把它显示到目标列布局中
@@ -54,20 +56,28 @@ export const reOpenWebView = function(dependencyTreeData: DependencyTreeData) {
 	} else {
 		createWebviewPanel();
 		createView();
+		const folderPath = getCurrentFolderPath();
+		const entryFilePath = getEntryFilePath();
+		if (folderPath) {
+			statusMsgGetFolderPath.postSuccess();
+		} else {
+			statusMsgGetFolderPath.postError();
+		}
+		if (entryFilePath) {
+			statusMsgGetEntryFile.postSuccess();
+		} else {
+			statusMsgGetEntryFile.postError();
+		}
 		openWebView(dependencyTreeData);
 	}
 };
-export const openWebView = function(dependencyTreeData: DependencyTreeData) {
+export const openWebView = function(dependencyTreeData: DependencyTreeData | undefined) {
 	const folderPath = getCurrentFolderPath();
-	if (folderPath) {
-		postMessageCatchError({ key: MESSAGE_DEPENDENCY_TREE_DATA, value: dependencyTreeData });
-		postMessageCatchError({ key: MESSAGE_FOLDER_PATH, value: folderPath });
-		postMessageCatchError({
-			key: MESSAGE_ASSETS_BASE_URL,
-			value: getBaseWebViewUri(),
-			description: ''
-		});
-	} else {
-		onError(NO_FOLDER);
-	}
+	postMessageCatchError({ key: MESSAGE_DEPENDENCY_TREE_DATA, value: dependencyTreeData });
+	postMessageCatchError({ key: MESSAGE_FOLDER_PATH, value: folderPath });
+	postMessageCatchError({
+		key: MESSAGE_ASSETS_BASE_URL,
+		value: getBaseWebViewUri(),
+		description: ''
+	});
 };
