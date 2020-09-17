d1e154b0e2027ec9b5b9509f81efa690e5dec289
sz-p <sz_p@outlook.com>
2020-09-18 00:00:00 +0800

    feat: update status view

--- src/webView/src/components/statusView/statusView.jsx
+++ src/webView/src/components/statusView/statusView.jsx
@@ -1,26 +1,43 @@
 import * as React from 'react';
 
+// import {DarkTheme} from '@fluentui/react';
 import { connect } from 'react-redux';
-
-import { statusList } from './statusList';
-
+import "./status.css"
+import { loadTheme } from '@fluentui/react';
+import { dark } from '../../utils/theme'
+import { StatusDoms } from './statusDom';
+import { useEffect, useState } from 'react'
+loadTheme(dark);
 const statusView = function (props) {
-  const { getDataStatus } = props;
-  const doms = [];
-  if (!getDataStatus) {
-    return null
-  } else {
-    for (let i = 0; i < getDataStatus; i++) {
-      doms.push(<div key={statusList[i]} >
-        {statusList[i]}
-      </div >)
+  let [doms, setDoms] = useState([]);
+  const { getDataStatus, viewHash } = props;
+  useEffect(() => {
+    if (viewHash) {
+      doms = [], setDoms(doms)
     }
-  }
-  return doms
+  }, [viewHash])
+  useEffect(() => {
+    if (getDataStatus) {
+      console.log(getDataStatus);
+      console.log(StatusDoms);
+      console.log(StatusDoms[getDataStatus.type])
+      console.log(getDataStatus.status)
+      setDoms(doms.concat(StatusDoms[getDataStatus.type][getDataStatus.status]))
+    }
+  }, [getDataStatus])
+
+  // if (getDataStatus) {
+
+  //   // setDoms(doms)
+  // }
+  return (
+    <div className="statusView">{doms}</div>
+  )
 }
 const mapStateToProps = (state) => {
   return {
-    getDataStatus: state.getDataStatus
+    getDataStatus: state.getDataStatus,
+    viewHash: state.viewHash
   }
 };
 export const StatusView = connect(mapStateToProps)(statusView);
