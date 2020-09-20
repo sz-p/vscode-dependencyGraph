1d1a02dd35ce389cdfa3fadf8967e81405c00224
sz-p <sz_p@outlook.com>
2020-09-21 00:00:00 +0800

    feat: vebview set entry file botton

--- src/webView/src/components/statusView/errorDom.jsx
+++ src/webView/src/components/statusView/errorDom.jsx
@@ -3,33 +3,33 @@ import "./status.css"
 import { connect } from 'react-redux';
 import { PrimaryButton, TextField } from 'office-ui-fabric-react';
 import * as React from 'react';
+import { useState } from 'react'
 // TODO i18n
-import { msgOpenFolder } from '../../utils/messages'
+import { msgOpenFolder, msgSetEntryFile } from '../../utils/messages'
 const openFolder = function () {
   msgOpenFolder.post();
 }
-const setEntryFile = function () {
-  window.console.log('setEntryFile');
+const setEntryFile = function (entryFilePath) {
+  msgSetEntryFile(entryFilePath).post()
 }
 const errorDom = function (props) {
-  const { folderPath, entryFilePath } = props;
-  console.log(folderPath);
-  console.count("render errorDom")
+  const { folderPath, defaultEntryFilePath } = props;
+  let [entryFilePath, setEntryFilePath] = useState(defaultEntryFilePath);
   return (<div className="statusView-errorArea">
     <div>
       <TextField className="statusView-inputBox" label="Folder" disabled required value={folderPath} />
       <PrimaryButton className="statusView-button" text="Open folder" onClick={openFolder} />
     </div>
     <div>
-      <TextField className="statusView-inputBox" label="Entry file" required prefix={folderPath} value={entryFilePath} />
-      <PrimaryButton className="statusView-button" text="Set entry file" onClick={setEntryFile} />
+      <TextField className="statusView-inputBox" label="Entry file" required prefix={folderPath} onChange={(e, v) => { console.log(e); console.log(v); setEntryFilePath(v) }} value={entryFilePath} />
+      <PrimaryButton className="statusView-button" text="Set entry file" onClick={() => setEntryFile(entryFilePath)} />
     </div>
   </div>)
 }
 const mapStateToProps = (state) => {
   return {
     folderPath: state.folderPath,
-    entryFilePath: state.entryFilePath
+    defaultEntryFilePath: state.entryFilePath,
   }
 };
 export const ErrorDom = connect(mapStateToProps)(errorDom);
