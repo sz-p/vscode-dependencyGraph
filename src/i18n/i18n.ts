7e44b535d848e1cd9bafc841ed5bd5c5e427a81b
sz-p <sz_p@outlook.com>
2020-09-22 00:00:00 +0800

    feat: get i18n from vscode.env.language

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,8 +1,9 @@
+import * as vscode from 'vscode';
 import { zhcn } from './zh-cn/zh-cn';
 import { engl } from './en-gl/en-gl';
-
+export const getCurrentLanguage = (): string => vscode.env.language;
+let LANGUAGE = getCurrentLanguage();
 export function getText(text: string) {
-	const LANGUAGE = 'en-gl';
 	if (LANGUAGE === 'en-gl') {
 		return engl[text];
 	} else if (LANGUAGE === 'zh-cn') {
