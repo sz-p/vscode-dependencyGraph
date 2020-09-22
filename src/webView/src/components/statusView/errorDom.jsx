b7db310623173ea34ac7f6384271a8066a07a036
sz-p <sz_p@outlook.com>
2020-09-22 10:29:57 +0800

    feat: on status view enter 'enter' to post entry file path message

--- src/webView/src/components/statusView/errorDom.jsx
+++ src/webView/src/components/statusView/errorDom.jsx
@@ -3,7 +3,7 @@ import "./status.css"
 import { connect } from 'react-redux';
 import { PrimaryButton, TextField } from 'office-ui-fabric-react';
 import * as React from 'react';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // TODO i18n
 import { msgOpenFolder, msgSetEntryFile } from '../../utils/messages'
 const openFolder = function () {
@@ -12,16 +12,30 @@ const openFolder = function () {
 const setEntryFile = function (entryFilePath) {
   msgSetEntryFile(entryFilePath).post()
 }
+const onEnter = function (entryFilePath) {
+  return function (key) {
+    console.log(entryFilePath)
+    console.log(key)
+    if (key.key === "Enter" && entryFilePath !== undefined) {
+      setEntryFile(entryFilePath)
+    }
+  }
+}
 const errorDom = function (props) {
   const { folderPath, defaultEntryFilePath } = props;
   let [entryFilePath, setEntryFilePath] = useState(defaultEntryFilePath);
+  useEffect(() => {
+    const onKeyPress = onEnter(entryFilePath)
+    document.addEventListener("keypress", onKeyPress);
+    return () => document.removeEventListener("keypress", onKeyPress)
+  }, [entryFilePath])
   return (<div className="statusView-errorArea">
     <div>
       <TextField className="statusView-inputBox" label="Folder" disabled required value={folderPath} />
       <PrimaryButton className="statusView-button" text="Open folder" onClick={openFolder} />
     </div>
     <div>
-      <TextField className="statusView-inputBox" label="Entry file" required prefix={folderPath} onChange={(e, v) => { console.log(e); console.log(v); setEntryFilePath(v) }} value={entryFilePath} />
+      <TextField className="statusView-inputBox" label="Entry file" required prefix={folderPath} onChange={(e, v) => { setEntryFilePath(v) }} value={entryFilePath} />
       <PrimaryButton className="statusView-button" text="Set entry file" onClick={() => setEntryFile(entryFilePath)} />
     </div>
   </div>)
