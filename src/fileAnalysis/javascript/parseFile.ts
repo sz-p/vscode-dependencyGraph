9049dc01d6a47f61bbf2548fe92088dc3333f1e8
sz-p <sz_p@outlook.com>
2020-10-30 00:00:00 +0800

    refactor: move get file introduction in parseFile.ts

--- src/fileAnalysis/javascript/parseFile.ts
+++ src/fileAnalysis/javascript/parseFile.ts
@@ -1,26 +1,53 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { pathExists } from '../../utils/utils';
+const getIntroduction = function(codeString: string) {
+	const reg = /@introduction (.*)\n/;
+	const result = codeString.match(reg);
+	if (result) {
+		return result[1].replace('\r', '');
+	} else {
+		return undefined;
+	}
+};
+const getDescription = function(codeString: string) {
+	const reg = /@description (.*)\n/;
+	const result = codeString.match(reg);
+	if (result) {
+		return result[1].replace('\r', '');
+	} else {
+		return undefined;
+	}
+};
 export interface FileAttribute {
 	codeString: string;
 	extname: string;
-	filename: string;
+	baseName: string;
 	absolutePath: string;
 	relativePath: string;
+	description: string | undefined;
+	introduction: string | undefined;
+	dirName: string;
 }
 export const parseFile = function(filePath: string, folderPath: string): FileAttribute | undefined {
 	// TODO not only undefined is not existed
 	if (!pathExists(filePath)) return undefined;
-	const codeString = fs.readFileSync(filePath).toString();
-	const extname = path.extname(filePath);
-	const filename = path.basename(filePath);
 	const absolutePath = filePath;
-	const relativePath = filePath.replace(folderPath, '');
+	const extname = path.extname(filePath);
+	const baseName = path.basename(filePath);
+	const dirName = path.dirname(absolutePath);
+	const relativePath = path.relative(absolutePath, folderPath);
+	const codeString = fs.readFileSync(filePath).toString();
+	const description = getDescription(codeString);
+	const introduction = getIntroduction(codeString);
 	return {
 		codeString,
 		extname,
-		filename,
+		baseName,
 		absolutePath,
-		relativePath
+		relativePath,
+		description,
+		introduction,
+		dirName
 	};
 };
