355d47f7a02c6da4043138aed7fe23ea0ee04829
sz-p <sz_p@outlook.com>
2020-10-29 11:39:36 +0800

    feat: parseVueCode

--- src/fileAnalysis/javascript/getAST/getAST.ts
+++ src/fileAnalysis/javascript/getAST/getAST.ts
@@ -1,10 +1,14 @@
 import { File } from '@babel/types';
 import { parseJsCode } from './parseJsCode';
+import { parseVueCode } from './parseVueCode';
 export const getAST = function(extname: string, codeString: string): File | undefined {
 	let ast = undefined;
 	switch (extname) {
 		case '.js':
 			ast = parseJsCode(codeString);
+			break;
+		case '.vue':
+			ast = parseVueCode(codeString);
 			break;
 	}
 	return ast;
---
+++ src/fileAnalysis/javascript/getAST/parseVueCode.ts
@@ -0,0 +1,11 @@
+import { File } from '@babel/types';
+import * as vueTemplateCompiler from 'vue-template-compiler';
+import { parseJsCode } from './parseJsCode';
+export const parseVueCode = function(vueString: string): File | undefined {
+	const compileResult = vueTemplateCompiler.parseComponent(vueString);
+	if (compileResult.script && compileResult.script.content) {
+		return parseJsCode(compileResult.script.content);
+	} else {
+		return undefined;
+	}
+};
