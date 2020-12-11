81bbe2408930424213a0c1f24304965699808e86
sz-p <sz_p@outlook.com>
2020-12-11 19:13:59 +0800

    feat: change get dependency tree as a module

--- src/fileAnalysis/javascript/parseJavascript.ts
+++ src/fileAnalysis/javascript/parseJavascript.ts
@@ -1,12 +1,43 @@
-import { DependencyTreeData } from "../../data-dependencyTree/dependencyTreeData.d";
+import {
+  DependencyTreeData,
+  FunctionInformation,
+} from "../../data-dependencyTree/dependencyTreeData.d";
+import { getFunctionInformation } from "./getFunctionInformation";
+import { i18n } from "../../i18n/i18n";
+import { getFileIconNameByFileName } from "../../utils/fileIcons/getFileIcon";
 
-export const onGetJsFileString = function (
-  dependencyNode: DependencyTreeData,
-  absolutePath: string,
-  codeString: string
-) {};
+import { visit } from "recast";
+import {
+  CIRCULAR_STRUCTURE_NODE_INTRODUCTION,
+  CIRCULAR_STRUCTURE_NODE_DESCRIPTION,
+} from "../../i18n/types";
+
 export const onGotJsAST = function (
   dependencyNode: DependencyTreeData,
   absolutePath: string,
   AST: any
-) {};
+) {
+  const functionsList = [] as FunctionInformation[];
+  dependencyNode.analysed = true;
+  dependencyNode.lines = AST.loc?.end.line;
+  visit(AST, {
+    visitFunction(nodePath) {
+      const functionInfo = getFunctionInformation(nodePath);
+      functionsList.push(functionInfo);
+      return false;
+    },
+  });
+  dependencyNode.functions = functionsList;
+};
+
+export const onGetJsCircularStructureNode = function (
+  dependencyNode: DependencyTreeData
+) {
+  const circularStructureNode = dependencyNode.children[0];
+  circularStructureNode.type = getFileIconNameByFileName(circularStructureNode.name);
+  circularStructureNode.analysed = false;
+  circularStructureNode.fileDescription = {
+    introduction: i18n.getText(CIRCULAR_STRUCTURE_NODE_INTRODUCTION),
+    description: i18n.getText(CIRCULAR_STRUCTURE_NODE_DESCRIPTION),
+  };
+};
