40fcc45aaee8b5dc8d7658be8a06cb623e886f5e
sz-p <sz_p@outlook.com>
2020-12-30 00:00:00 +0800

    feat: setting view

--- src/webView/src/components/settingView/settingView.jsx
+++ src/webView/src/components/settingView/settingView.jsx
@@ -1,17 +1,31 @@
 import * as React from "react";
 
 import { connect } from "react-redux";
+import "./settingView.css";
+import { FontIcon } from "office-ui-fabric-react/lib/Icon";
+import { action_changeSettingStatus } from "../../actions/action";
+import { store } from "../../reducers/store";
 
 const settingView = function (props) {
-  const { getDataFailed } = props;
-  if (!getDataFailed) {
-    return null;
-  }
-  return <div>settingView</div>;
+  const { showSetting } = props;
+  console.log(showSetting);
+  return (
+    <div
+      className="settingView"
+      style={{ display: showSetting ? "block" : "none" }}
+    >
+      <FontIcon
+        iconName="ChromeClose"
+        onClick={() => {
+          store.dispatch(action_changeSettingStatus());
+        }}
+      />
+    </div>
+  );
 };
 const mapStateToProps = (state) => {
   return {
-    getDataFailed: state.getDataFailed,
+    showSetting: state.showSetting,
   };
 };
 export const SettingView = connect(mapStateToProps)(settingView);
