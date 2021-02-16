cd45a2628903d5e0998c52c5f81f4dea3540a9da
sz-p <SZ_P@outlook.com>
2021-02-16 11:42:01 +0800

    fix: show updateButton when get savedData

---
+++ tests/extensionTester/dataTester/data.test.ts
@@ -0,0 +1,21 @@
+import { isSavedData } from "../../../src/utils/data/data";
+import { expect } from 'chai';
+import * as path from "path";
+
+// base on "vscode" can't be test
+
+// const testCase0 = path.resolve(__dirname, `./testData0/`)
+// const testCase1 = path.resolve(__dirname, `./testData1/`)
+// const testCase2 = path.resolve(__dirname, `./testData2/`)
+
+// describe("saveData", function () {
+//   it("isSavedData: savedData", function () {
+//     expect(isSavedData(testCase0)).to.true;
+//   });
+//   it("isSavedData: no Dat", function () {
+//     expect(isSavedData(testCase1)).to.false;
+//   });
+//   it("isSavedData: no framegraph folder", function () {
+//     expect(isSavedData(testCase2)).to.false;
+//   });
+// });
